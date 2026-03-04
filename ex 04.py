b46a7ab8a3a75f065be56fad40e52f4f36f4b4fd
allan <zallan396@gmail.com>
2026-03-03 21:04:35 -0300

    Update ex 04.py

--- ex 04.py
+++ ex 04.py
@@ -2,11 +2,12 @@ print("Desconto em compras")
 
 valor = float(input("Digite o valor da compra:"))
 
-if valor >= 100:
+if valor > 100:
    desconto = (valor *0.9)
 
    print ("Preço final com desconto:",desconto)
 
 else :
      print ("Preço final:", valor)
+
   
