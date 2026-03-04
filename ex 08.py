8a6f6c7bb1bcdf57f236cc8f5fa08eb759e08933
allan <zallan396@gmail.com>
2026-03-03 22:17:53 -0300

    ex 08.py

--- ex 08.py
+++ ex 08.py
@@ -4,11 +4,13 @@ numero = int(input("Digite um numero:"))
 
 while  numero > 0  :
     if numero %2 !=0 :
-
-        print (numero)
         numero -=1
         print("Número par")
     else:
      print(numero)
      numero -=2
      print("Número par")
+        if numero == 0 :
+         print(numero)    
+    if numero == 0 :
+         print(numero)
